befaeb6ae1c0df4e01170d6bb69cb04ad2ee430f
vkmita <me@vkmita.com>
2015-06-23 13:02:27 -0700

    add a knowmad_copyright_footer_text method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # >> puts Benchmark.measure { Time.now.year }
+  # 0.000000   0.000000   0.000000 (  0.000026)
+  # It's .02ms, I'd rather never to wory about this, or overengineer it.
+  def knowmad_copyright_footer_text
+    "&copy; Knowmad #{Time.now.year}"
+  end
 end
