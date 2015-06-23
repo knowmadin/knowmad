1113f6815c2b722bcc5eb9cd5fd15bdcfd2d3a57
vkmita <me@vkmita.com>
2015-06-23 14:46:44 -0700

    add 120 max line length rubocop rule and fix indentation in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   devise_for :users, path: "auth", path_names: {
-      sign_in: 'sign_in',
-      sign_out: 'sign_out',
-      password: 'secret',
-      confirmation: 'verification',
-      registration: 'register',
-      sign_up: 'cmon_let_me_in'
-    }
+    sign_in: 'sign_in',
+    sign_out: 'sign_out',
+    password: 'secret',
+    confirmation: 'verification',
+    registration: 'register',
+    sign_up: 'cmon_let_me_in'
+  }
 end
