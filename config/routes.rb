e96950b6cb284981d49623f740b2aee5479ded4a
vkmita <me@vkmita.com>
2015-06-23 12:23:48 -0700

    don't prepend users onto auth stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  devise_for :users
+  devise_for :users, path: "auth", path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      registration: 'register',
+      sign_up: 'cmon_let_me_in'
+    }
 end
