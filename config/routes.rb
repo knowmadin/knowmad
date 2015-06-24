72269515dcec713cbed85a4a53111290617c4805
vkmita <me@vkmita.com>
2015-06-23 18:08:02 -0700

    make password route nicer

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', path_names: {
     sign_in:      'sign_in',
     sign_out:     'sign_out',
-    password:     'secret',
     registration: 'account'
   }
 end
