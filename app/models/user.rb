75c60f1433850e86c9e8f6caffbf34daba3c781d
vkmita <me@vkmita.com>
2015-06-24 16:15:45 -0700

    remove timeoutable and change stings in i18n files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-    :timeoutable, :omniauthable
+    :omniauthable
 end
