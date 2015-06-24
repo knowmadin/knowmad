6858224eb4c09ba46e50975945f5236a56fd6671
vkmita <me@vkmita.com>
2015-06-24 16:42:01 -0700

    try to use custom device registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   devise_for :users, path: 'auth', path_names: {
-    sign_in:      'sign_in',
-    sign_out:     'sign_out',
-    registration: 'account'
-  }
+      sign_in:      'sign_in',
+      sign_out:     'sign_out',
+      registration: 'account'
+    } do
+      post 'account', to: "users/registrationss#create"
+    end
 end
