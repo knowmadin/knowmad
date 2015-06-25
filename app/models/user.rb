7789feae4b411774722276676a7cabc3f4c97de8
vkmita <me@vkmita.com>
2015-06-25 13:03:07 -0700

    add comment to method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
     :omniauthable
 
+  # override devise method, so that enqueue all smpt calls instead of sending them now
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
