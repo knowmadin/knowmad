cd7300dabf809b2d5caddf889c59f7f3951dd882
vkmita <me@vkmita.com>
2015-06-24 17:53:47 -0700

    Enqueue all devise emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
     :omniauthable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
