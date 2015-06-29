3be4999ea94d3311c7222908e581b392038c6b2c
vkmita <me@vkmita.com>
2015-06-29 15:51:35 -0700

    change that comment some more

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
     :omniauthable, omniauth_providers: [:facebook]
 
-  # override users method, so that enqueue all devise emails
+  # enqueue all devise emails
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
