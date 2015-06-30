e2bcb58301c4da055a2e1abf94115137332e649d
vkmita <me@vkmita.com>
2015-06-29 19:54:13 -0700

    add the the oauth providers to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-    :omniauthable, omniauth_providers: [:facebook]
+    :omniauthable, omniauth_providers: [:facebook, :twitter, :google_oauth2]
 
   # enqueue all devise emails
   def send_devise_notification(notification, *args)
