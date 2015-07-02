79d1f3be096195ad6eee70fa378f189f2452a31b
vkmita <me@vkmita.com>
2015-07-01 17:11:35 -0700

    dont use v2 of graph api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], client_options: {
-    :site => 'https://graph.facebook.com/v2.0',
-    :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
-  }
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 end
