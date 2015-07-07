f915f321173607aa8e54bbfcf01d14a32b19bde7
vkmita <me@vkmita.com>
2015-07-06 17:19:45 -0700

    use the twitter identity initializer in the controller

--- app/controllers/identities/social/omniauth_callbacks_controller.rb
+++ app/controllers/identities/social/omniauth_callbacks_controller.rb
@@ -12,7 +12,13 @@ class Identities::Social::OmniauthCallbacksController < Devise::OmniauthCallback
   end
 
   def twitter
-    render 'home/index'
+    if twitter_identity.errors.any?
+      flash[:error] = twitter_identity.errors.full_messages.to_sentence
+      redirect_to new_email_identity_session_path
+    else
+      sign_in(:twitter_identity, twitter_identity)
+      redirect_to controller: '/home', action: 'index', anchor: nil
+    end
   end
 
   def google_oauth2
