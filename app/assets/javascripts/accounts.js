3b590a9ffdc3a39b1fc212135b2e16529722a7c1
vkmita <me@vkmita.com>
2015-07-08 18:24:31 -0700

    Use a simpler query approach

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,2 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$('.subdomain-field').on('keyup focus', function() {
+  $('.mirrorInput').val($('.realInput').val() + ".knowmad.in");
+});
