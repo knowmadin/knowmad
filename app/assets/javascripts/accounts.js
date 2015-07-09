73cf70fe0ea362e441b5bd646c28571c04b75cb6
vkmita <me@vkmita.com>
2015-07-08 19:06:15 -0700

    add back the original placeholder on blur when no text has been inputed

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,8 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var subdomainField = $('.subdomain-field');
+var subdomainField = $('.subdomain-field'),
+  mirrorInput = $('.mirror-input');
 
 subdomainField.on('keyup focus paste', function() {
   subdomainField.removeAttr('placeholder');
-  $('.mirror-input').val(subdomainField.val() + '.knowmad.in');
+  mirrorInput.val(subdomainField.val() + '.knowmad.in');
+});
+
+subdomainField.on('blur', function() {
+  debugger;
+  if (subdomainField.val().length === 0) {
+    debugger;
+    subdomainField.attr('placeholder', 'subdomain.knowmad.in');
+    mirrorInput.val('');
+  }
 });
