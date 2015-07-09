c6b5b811bbba0ec973edf855ae1aee8a2134a43e
vkmita <me@vkmita.com>
2015-07-08 20:50:45 -0700

    create a current_identity method in ApplicationController and create the create action in AccountsController

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,20 @@
 class AccountsController < ApplicationController
+
+
+  def create
+    if account.save
+      current_identity.account = account
+      current_identity.save
+      redirect_to controller: '/home', action: 'index'
+    else
+      flash[:error] = resource.errors.full_messages.to_sentence
+      render 'accounts/new'
+    end
+  end
+
+  private
+
+  def account
+    @account ||= Account.new(subdomain: params[:subdomain])
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # gotta be a better way of doing this
+  def current_identity
+    @current_identity ||= if email_identity_signed_in?
+      current_email_identity
+    elsif twitter_identity_signed_in?
+      current_twitter_identity
+    elsif facebook_identity_signed_in?
+      current_facebook_identity
+    elsif google_identity_signed_in?
+      current_google_identity
+    end
+  end
 end
