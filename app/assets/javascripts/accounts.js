fa94a7e54b4102db4f0ed0d59b8f6cfd835f7b3b
vkmita <me@vkmita.com>
2015-07-08 18:31:28 -0700

    Fix class names and css

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,5 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$('.subdomain-field').on('keyup focus', function() {
-  $('.mirrorInput').val($('.realInput').val() + ".knowmad.in");
+var subdomainField = $('.subdomain-field');
+
+subdomainField.on('keyup focus', function() {
+  $('.mirror-input').val(subdomainField.val() + '.knowmad.in');
 });
