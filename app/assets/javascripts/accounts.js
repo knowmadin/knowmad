aadf5b53bf850759c101383dd681b49b13295894
vkmita <me@vkmita.com>
2015-07-10 18:49:37 -0700

    better experience when there is some kind of vlaidation error on subdomain

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,18 +1,20 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-var subdomainField = $('.subdomain-field'),
-  mirrorInput = $('.mirror-input');
+$(document).ready(function() {
+  var subdomainField = $('.subdomain-field'),
+    mirrorInput = $('.mirror-input');
 
-subdomainField.on('keyup focus paste', function() {
-  subdomainField.removeAttr('placeholder');
-  mirrorInput.val(subdomainField.val() + '.knowmad.in');
-});
+  subdomainField.on('keyup focus paste', function() {
+    subdomainField.removeAttr('placeholder');
+    mirrorInput.val(subdomainField.val() + '.knowmad.in');
+  });
 
-subdomainField.on('blur', function() {
-  debugger;
-  if (subdomainField.val().length === 0) {
-    debugger;
-    subdomainField.attr('placeholder', 'subdomain.knowmad.in');
-    mirrorInput.val('');
+  subdomainField.on('blur', function() {
+    if (!subdomainField.val().length) {
+      subdomainField.attr('placeholder', 'subdomain.knowmad.in');
+      mirrorInput.val('');
+    }
+  });
+
+  if (subdomainField.val().length) {
+    mirrorInput.val(subdomainField.val() + '.knowmad.in');
   }
 });
