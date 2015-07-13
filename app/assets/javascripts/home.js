d194d555a4887d08572568179f1a4de3d2e656c5
vkmita <me@vkmita.com>
2015-07-13 13:33:58 -0700

    change the default fill color to the knowmad blue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,30 +5,9 @@ $(document).ready(function() {
     element: document.getElementById("basic_choropleth"),
     projection: 'mercator',
     fills: {
-      defaultFill: "#ABDDA4",
-      authorHasTraveledTo: "#fa0fa0"
-    },
-    data: {
-      USA: { fillKey: "authorHasTraveledTo" },
-      JPN: { fillKey: "authorHasTraveledTo" },
-      ITA: { fillKey: "authorHasTraveledTo" },
-      CRI: { fillKey: "authorHasTraveledTo" },
-      KOR: { fillKey: "authorHasTraveledTo" },
-      DEU: { fillKey: "authorHasTraveledTo" }
+      defaultFill: "#428bca"
     }
   });
 
   var colors = d3.scale.category10();
-
-  window.setInterval(function() {
-    basic_choropleth.updateChoropleth({
-      USA: colors(Math.random() * 10),
-      RUS: colors(Math.random() * 100),
-      AUS: { fillKey: 'authorHasTraveledTo' },
-      BRA: colors(Math.random() * 50),
-      CAN: colors(Math.random() * 50),
-      ZAF: colors(Math.random() * 50),
-      IND: colors(Math.random() * 50)
-    });
-  }, 2000);
 });
