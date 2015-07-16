445aa1ea4a415b961b1dbec63658d42d5020ccd2
vkmita <me@vkmita.com>
2015-07-15 18:42:57 -0700

    add a white glow to the search bar and log the pointer position and click position to the console

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-
   L.mapbox.accessToken = 'pk.eyJ1IjoidmttaXRhIiwiYSI6IjE1ZWZiZDhhMTI1NzJjMTA2ZWRhOGRmOTBkYTJmOTRlIn0.UQVq8Xf_O6V1G8eFCRU3SQ';
-  L.mapbox.map('map', 'vkmita.0770a998');
+  var map = L.mapbox.map('map', 'vkmita.0770a998');
 
+  mao.on('mousemove click', function(e) {
+    console.log(e.containerPoint.toString() + ', ' + e.latlng.toString())
+  });
 });
