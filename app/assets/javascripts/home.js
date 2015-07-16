535639156a85edd19958f9d4911368fa0364a681
vkmita <me@vkmita.com>
2015-07-15 18:45:28 -0700

    lol map instead of mao

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoidmttaXRhIiwiYSI6IjE1ZWZiZDhhMTI1NzJjMTA2ZWRhOGRmOTBkYTJmOTRlIn0.UQVq8Xf_O6V1G8eFCRU3SQ';
   var map = L.mapbox.map('map', 'vkmita.0770a998');
 
-  mao.on('mousemove click', function(e) {
+  map.on('mousemove click', function(e) {
     console.log(e.containerPoint.toString() + ', ' + e.latlng.toString())
   });
 });
