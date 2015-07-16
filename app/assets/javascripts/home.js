6e7169bf179db9c967247be7e046cccbf7758ee1
vkmita <me@vkmita.com>
2015-07-15 20:14:12 -0700

    only log the letlong when there 1 click in 200 milliseconds

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoidmttaXRhIiwiYSI6IjE1ZWZiZDhhMTI1NzJjMTA2ZWRhOGRmOTBkYTJmOTRlIn0.UQVq8Xf_O6V1G8eFCRU3SQ';
-  var map = L.mapbox.map('map', 'vkmita.0770a998');
 
-  map.on('mousemove click', function(e) {
-    console.log(e.containerPoint.toString() + ', ' + e.latlng.toString())
-  });
+  var map = L.mapbox.map('map', 'vkmita.0770a998'),
+      numberOfClicks = 0;
+
+  map.on('click', click);
+
+  function click(e) {
+    if (numberOfClicks == 0) {
+      setTimeout(reaction, 200);
+    }
+
+    numberOfClicks = numberOfClicks + 1;
+  }
+
+  function reaction() {
+    if (numberOfClicks === 1) {
+      console.log(e.containerPoint.toString() + ', ' + e.latlng.toString())
+    } else {
+      console.log(numberOfClicks.toString())
+    }
+
+    numberOfClicks = 0;
+  }
 });
