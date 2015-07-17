238e348f0471b77a04d39968ac99a29d1bce3f98
vkmita <me@vkmita.com>
2015-07-17 15:12:55 -0700

    remove mapbox geocoder, only use googles

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,6 @@ if ($('#map')) {
     L.mapbox.accessToken = 'pk.eyJ1IjoidmttaXRhIiwiYSI6IjE1ZWZiZDhhMTI1NzJjMTA2ZWRhOGRmOTBkYTJmOTRlIn0.UQVq8Xf_O6V1G8eFCRU3SQ';
 
     var map = L.mapbox.map('map', 'vkmita.0770a998'),
-      geocoder = L.mapbox.geocoder('mapbox.places'),
       numberOfClicks = 0;
 
     map.on('click', click);
@@ -18,24 +17,13 @@ if ($('#map')) {
 
     function reaction(e, geocoder, map) {
       if (numberOfClicks === 1) {
-        geocoder.reverseQuery(e.latlng, function(err, result) {
-          if (err) {
-            alert('oops something went wrong with getting the location' + err.toString());
-          } else {
-            var context = result.features[0].context;
-            for (var i = 0, l = context.length; i < l; i++) {
-              console.log(context[i]);
-            }
+        var googleGeocodeApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCTcnA2Fr6ZH97xLLN94J5tIP3Rab_HyQ4&latlng=' + e.latlng.lat.toString() + ',' + e.latlng.lng.toString();
 
-            var googleGeocodeApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCTcnA2Fr6ZH97xLLN94J5tIP3Rab_HyQ4&latlng=' + e.latlng.lat.toString() + ',' + e.latlng.lng.toString();
-
-            $.ajax({
-              url: googleGeocodeApiUrl
-            }).done(function(json) {
-              console.log(json);
-            });
-          }
-        })
+        $.ajax({
+          url: googleGeocodeApiUrl
+        }).done(function(json) {
+          json;
+        });
       } else {
         console.log(numberOfClicks.toString())
       }
