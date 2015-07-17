700d30e3df88f02da9aece5c3c016f3cffd6012b
vkmita <me@vkmita.com>
2015-07-16 22:02:23 -0700

    let's figure out where we are

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,21 +2,23 @@ $(document).ready(function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoidmttaXRhIiwiYSI6IjE1ZWZiZDhhMTI1NzJjMTA2ZWRhOGRmOTBkYTJmOTRlIn0.UQVq8Xf_O6V1G8eFCRU3SQ';
 
   var map = L.mapbox.map('map', 'vkmita.0770a998'),
+      geocoder = L.mapbox.geocoder('mapbox.places'),
       numberOfClicks = 0;
 
   map.on('click', click);
 
   function click(e) {
     if (numberOfClicks === 0) {
-      setTimeout(reaction.bind(this, e), 500);
+      setTimeout(reaction.bind(this, e, geocoder), 500);
     }
 
     numberOfClicks = numberOfClicks + 1;
   }
 
-  function reaction(e) {
+  function reaction(e, geocoder) {
     if (numberOfClicks === 1) {
-      console.log(e.containerPoint.toString() + ', ' + e.latlng.toString())
+      debugger;
+      console.log(e.latlng)
     } else {
       console.log(numberOfClicks.toString())
     }
