f5967a9a98f45870ed35e1b446f4dac6d7b5177c
vkmita <me@vkmita.com>
2015-07-17 22:43:13 -0700

    change the div from a bootrap one to a neutral one

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -51,7 +51,7 @@ if ($('#map')) {
             }
           }
 
-          $('.modal-body').html(modalBody + '</ul>');
+          $('.address-components').html(modalBody + '</ul>');
           $('#address-modal').modal();
         });
       } else {
